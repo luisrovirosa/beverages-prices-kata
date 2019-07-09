71ed59453247b557b3e8fd6e86a4e116c33c3c59
Luis Rovirosa <luisrovirosa@gmail.com>
2019-07-09 20:03:40 +0200

    remove unnecessary factory

--- src/test/java/unit_tests/BeveragesPricingTest.java
+++ src/test/java/unit_tests/BeveragesPricingTest.java
@@ -1,6 +1,11 @@
 package unit_tests;
 
 import beverages.*;
+import beverages.drink.Coffee;
+import beverages.drink.HotChocolate;
+import beverages.drink.Tea;
+import beverages.extra.Cream;
+import beverages.extra.Milk;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -10,43 +15,48 @@ import static org.hamcrest.Matchers.closeTo;
 public class BeveragesPricingTest {
     @Test
     public void computes_coffee_price() {
-        Beverage coffee = new BeverageFactory().coffee();
+        Beverage coffee = new Coffee();
         assertThat(coffee.price(), is(closeTo(1.20, 0.001)));
     }
 
     @Test
     public void computes_tea_price() {
-        Beverage tea = new BeverageFactory().tea();
+        Beverage tea = new Tea();
         assertThat(tea.price(), is(closeTo(1.50, 0.001)));
     }
 
     @Test
     public void computes_hot_chocolate_price() {
-        Beverage hotChocolate = new BeverageFactory().hotChocolate();
+        Beverage hotChocolate = new HotChocolate();
         assertThat(hotChocolate.price(), is(closeTo(1.45, 0.001)));
     }
 
     @Test
     public void computes_tea_with_milk_price() {
-        Beverage teaWithMilk = new BeverageFactory().teaWithMilk();
+        Beverage teaWithMilk = new Tea()
+                .with(new Milk());
         assertThat(teaWithMilk.price(), is(closeTo(1.60, 0.001)));
     }
 
     @Test
     public void computes_coffee_with_milk_price() {
-        Beverage coffeeWithMilk = new BeverageFactory().coffeeWithMilk();
+        Beverage coffeeWithMilk = new Coffee()
+                .with(new Milk());
         assertThat(coffeeWithMilk.price(), is(closeTo(1.30, 0.001)));
     }
 
     @Test
     public void computes_coffee_with_milk_and_cream_price() {
-        Beverage coffeeWithMilkAndCream = new BeverageFactory().coffeeWithMilkAndCream();
+        Beverage coffeeWithMilkAndCream = new Coffee()
+                .with(new Milk())
+                .with(new Cream());
         assertThat(coffeeWithMilkAndCream.price(), is(closeTo(1.45, 0.001)));
     }
 
     @Test
     public void computes_hot_chocolate_with_cream_price() {
-        Beverage hotChocolateWithCream = new BeverageFactory().hotChocolateWithCream();
+        Beverage hotChocolateWithCream = new HotChocolate()
+                .with(new Cream());
         assertThat(hotChocolateWithCream.price(),  is(closeTo(1.60, 0.001)));
     }
 }
